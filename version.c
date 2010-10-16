5826e8025ebb43d19617f88daf32c98748df803b
John Plevyak <jplevyak@acm.org>
2010-10-16 14:37:09 -0700

    version is now git signature

--- version.c
+++ version.c
@@ -6,7 +6,7 @@
 void
 d_version(char *v) {
   v += sprintf(v, "%d.%d", D_MAJOR_VERSION, D_MINOR_VERSION);
-  if (D_BUILD_VERSION)
-    v += sprintf(v, ".%d", D_BUILD_VERSION);
+  if (strcmp("",D_BUILD_VERSION))
+    v += sprintf(v, ".%s", D_BUILD_VERSION);
 }
 
