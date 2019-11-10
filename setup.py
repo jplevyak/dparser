adbd855ce0f42b3f3c9eaa0acd1146c767f216bc
John Plevyak <jplevyak@apache.org>
2019-11-10 12:22:18 -0800

    Fix compilation warnings and update python to use setuptools.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from distutils.command.build import build
+
+
+# Sadly setuptools has a nasty bug whereby it doesn't swig
+# the extension module before trying to install the generated
+# .py file.
+class build_alt_order(build, object):
+    def __init__(self, *args):
+        super(build_alt_order, self).__init__(*args)
+        self.sub_commands = [('build_ext', build.has_ext_modules),
+                             ('build_py', build.has_pure_modules)]
 
 
 module_swigc = Extension(
@@ -8,16 +19,21 @@ module_swigc = Extension(
                  'dparse_tree.c', 'lr.c', 'read_binary.c',
                  'util.c', 'gram.c', 'mkdparse.c', 'scan.c',
                  'write_tables.c', 'grammar.g.c', 'symtab.c'],
+        swig_opts=['-modern', '-I..'],
         include_dirs=[".", 'python'],
-        define_macros=[('SWIG_GLOBAL', None)],
+        define_macros=[('SWIG_GLOBAL', None),
+                       ('SWIG_PYTHON_STRICT_BYTE_CHAR', None)],
         extra_compile_args=['-ggdb3', '-O3'])
 
 setup(
         name="dparser",
-        version="1.31.2",
+        version="1.31.8",
         description='DParser for Python',
-        py_modules=["dparser", "dparser_swigc"],
-        package_dir={"": 'python', 'dparser_swigc': 'python'},
         url='https://github.com/jplevyak/dparser',
+        author='John Plevyak',
+        author_email='jplevyak@gmail.com',
+        cmdclass={'build': build_alt_order},
         ext_modules=[module_swigc],
+        py_modules=["dparser", 'dparser_swigc'],
+        package_dir={'': 'python'},
 )
