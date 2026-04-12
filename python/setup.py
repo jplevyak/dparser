4c7eb96967daa8452155e5ae9f589da78a6b3bf8
John Plevyak <jplevyak@gmail.com>
2026-04-11 22:52:44 -0700

    Fixup python build system.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -1,23 +1,7 @@
-from distutils.core import setup, Extension
-from distutils.command.install_data import install_data
-
-# Pete Shinner's distutils data file fix... from distutils-sig
-# data installer with improved intelligence over distutils
-# data files are copied into the project directory instead
-# of willy-nilly
-
-
-class smart_install_data(install_data):
-    def __init__(self):
-        super(install_data).__init__()
-        self.install_dir = None
-
-    def run(self):
-        # need to change self.install_dir to the library dir
-        install_cmd = self.get_finalized_command('install')
-        self.install_dir = getattr(install_cmd, 'install_lib')
-        return install_data.run(self)
-
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
 
 module_swigc = Extension(
         '_dparser_swigc',
@@ -29,7 +13,6 @@ module_swigc = Extension(
 
 setup(
         name="dparser",
-        cmdclass={"install_data": smart_install_data},
         version="1.31",
         description='DParser for Python',
         py_modules=["dparser", "dparser_swigc"],
