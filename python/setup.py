1af24e39ab48c3f737a688da399ae6752e2aa56e
James Ouyang <JYOuyang@gmail.com>
2026-04-16 13:49:08 -0700

    pyproject: Move more stuff over.

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,7 @@
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from Cython.Build import cythonize
 
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    import sys
-    print("Error: Cython is required to build dparser python bindings.")
-    print("Please install it using: python3 -m pip install cython")
-    sys.exit(1)
-
-module_cython = Extension(
+module = Extension(
     'dparser',
     sources=['dparser.pyx'],
     libraries=['mkdparse', 'dparse'],
@@ -19,10 +9,4 @@ module_cython = Extension(
     extra_compile_args=['-Wall', '-O3', '-g']
 )
 
-setup(
-    name="dparser",
-    version="1.31",
-    description='DParser for Python',
-    url='https://github.com/jplevyak/dparser',
-    ext_modules=cythonize(module_cython, language_level="3")
-)
+setup(ext_modules=cythonize(module))
